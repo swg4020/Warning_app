c67aa982f60bf97656b6549a80196ea46ce6349a
swg4020 <swg4020@naver.com>
2024-05-02 17:42:30 +0900

    api

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 - Router / npm i react-router-dom
 - axios / npm install axios
 - query / npm i @tanstack/react-query
+
+## API
+API 이름 : 행정안전부_지진겸용 임시주거시설
+저작자 : 행정안전부
+출처 : https://www.data.go.kr/data/15021030/openapi.do#/API%20%EB%AA%A9%EB%A1%9D/getEarthquakeIndoors3List
--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiKey =
   "uzqzCV0OaR8IQR7UXErSXf++hcUKyQRCS2CT6q/LrQdoMNHPHkm3UtU5godlE8EyQMoTu5sY1nkbJKMWOouO6w==";
 
-//
+//행정안전부_지진겸용 임시주거시설 api
 const instanc = axios.create({
   baseURL: "https://apis.data.go.kr/1741000/EarthquakeIndoors3/",
   params: {
